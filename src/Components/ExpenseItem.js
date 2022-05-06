07e30b2ee688158e4176bcca46b22230fbddd79f
Suraj Kawadkar <suraj.kawadkar14@gmail.com>
2022-05-06 23:28:51 +0530

    date added

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,9 +1,17 @@
+import './ExpenseItem.css'
+
 function ExpenseItem(){
     return (
-        <div>
-           <div>car</div>
-           <h2>date</h2>
-           <div>$296</div>
+        <div className='expense-item'>
+           <div>March 28 2020</div>
+           <div className ='expense-item__description'>
+           <h2>purchased a car</h2>
+           <div className='expense-item__price'>$296</div>
+
+
+
+           </div>
+           
         </div>
     );
 }
